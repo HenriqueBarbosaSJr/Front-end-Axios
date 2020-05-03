0cd880efe7fdf24d19975ea248882b2f11679e86
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-03 20:55:15 -0300

    Front-end - Acessando dados de uma API / Cadastro, Consulta e Alteração

--- js/script.js
+++ js/script.js
@@ -6,8 +6,11 @@
 
 const formDados = document.getElementById('frmdados');
 const inputUser = document.getElementById('impName');
+const impId = document.getElementById('impId');
 const btnConst = document.getElementById('btnConst');
 const btnCad = document.getElementById('btnCad');
+const btnUpdate = document.getElementById('btnUpdate');
+
 
 
 btnConst.onclick = function(){
@@ -22,11 +25,27 @@ btnConst.onclick = function(){
 };
 
 btnCad.onclick = function(){
-    console.log('Botão Cadastro')
+    console.log('Botão Cadastro exercutado')
     var nameUser = inputUser.value;
     axios({
         method: 'POST',
         url: 'http://localhost:3333/users',
+        data: {
+            "username": nameUser,
+        }
+    }).then(res => {
+        console.log(res.data);
+    }).catch(err => console.log(err))
+
+};
+
+btnUpdate.onclick = function(){
+    console.log('Botão Updade executado')
+    var nameUser = inputUser.value;
+    var idupdate = impId.value;
+    axios({
+        method: 'put',
+        url: 'http://localhost:3333/users/' + idupdate,
         data: {
             "username": nameUser,
         }
